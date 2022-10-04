075e0767bcd3b1a5d5cb87393a21dcb443bd9be9
Deepak Singh <deepak.inbox229@gmail.com>
2022-10-04 23:36:16 +0530

    Initial Commit

---
+++ GitRepoLib/GitRepoLib.h
@@ -0,0 +1,18 @@
+//
+//  GitRepoLib.h
+//  GitRepoLib
+//
+//  Created by Deepak on 04/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitRepoLib.
+FOUNDATION_EXPORT double GitRepoLibVersionNumber;
+
+//! Project version string for GitRepoLib.
+FOUNDATION_EXPORT const unsigned char GitRepoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitRepoLib/PublicHeader.h>
+
+
